3b69555b3acfc1137ba8cc73e9fd07527e7309ea
Lisandro Gonzalez <sacragonza@gmail.com>
2023-02-02 22:23:24 -0300

    ejercicios 9 y 10 bis

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -1,13 +1,26 @@
 /*
 Lisandro Gonzalez
+9 BIS
 Debemos lograr tomar el importe por ID ,
 transformarlo a entero (parseInt), luego
-mostrar el importe con un aumento del 10 %
+mostrar el importe con un aumento ingresado por prompt
 en el cuadro de texto "RESULTADO".*/
-function mostrarAumento()
+/*function mostrarAumento()
 {
 	var sueldo = parseInt(document.getElementById("txtIdSueldo").value);
 	var inc = sueldo * 0.1;
 
 	document.getElementById("txtIdResultado").value = sueldo + inc;
+}*/
+function mostrarAumento()
+{
+	var porcentaje = prompt("Que porcentaje del sueldo quiere aumentar?");
+	var sueldo = document.getElementById("txtIdSueldo").value;
+
+	var sueldoNum = parseInt(sueldo);
+	var porcNum = parseInt(porcentaje);
+
+	var inc = sueldoNum * porcNum / 100;
+
+	document.getElementById("txtIdResultado").value = sueldoNum + inc;
 }
--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -1,14 +1,27 @@
 /*
 Lisandro Gonzalez
+10 BIS
 Debemos lograr tomar el importe por ID.
 Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
-function mostrarAumento()
+/*function mostrarAumento()
 {
 	// PRUEBA GIT
 	var nro = parseInt(document.getElementById("txtIdImporte").value);
 	var desc = nro * 0.25;
 
 	document.getElementById("txtIdResultado").value = nro - desc;
+}*/
+function mostrarAumento()
+{
+	var porcentaje = prompt("Que porcentaje del importe quiere decrementar?");
+	var importe = document.getElementById("txtIdImporte").value;
+
+	var porcNum = parseInt(porcentaje);
+	var importeNum = parseInt(importe);
+
+	var dec = importeNum * porcNum / 100;
+
+	document.getElementById("txtIdResultado").value = importeNum - dec;
 }
