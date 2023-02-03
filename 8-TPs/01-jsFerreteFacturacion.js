ca345f83282e5a6f5cfbd0246ff1de36aac0306e
Lisandro Gonzalez <sacragonza@gmail.com>
2023-02-03 19:44:26 -0300

    tp1 ferrete

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -5,13 +5,49 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+	var precio1;
+    var precio2;
+    var precio3;
+    var suma;
+
+    precio1 = parseInt(document.getElementById("txtIdPrecioUno").value);
+    precio2 = parseInt(document.getElementById("txtIdPrecioDos").value);
+    precio3 = parseInt(document.getElementById("txtIdPrecioTres").value);
+
+    suma = precio1 + precio2 + precio3;
+    
+    alert("La suma de los precios es: " + suma);
 }
 function Promedio () 
 {
-	
+    var precio1;
+    var precio2;
+    var precio3;
+    var promedio;
+
+    precio1 = parseInt(document.getElementById("txtIdPrecioUno").value);
+    precio2 = parseInt(document.getElementById("txtIdPrecioDos").value);
+    precio3 = parseInt(document.getElementById("txtIdPrecioTres").value);
+
+    promedio = (precio1 + precio2 + precio3) / 3;
+    
+    alert("El promedio es de: " + promedio);
 }
 function PrecioFinal () 
 {
-	
+    var precio1;
+    var precio2;
+    var precio3;
+    var precioFinal;
+    var iva;
+
+    precio1 = parseInt(document.getElementById("txtIdPrecioUno").value);
+    precio2 = parseInt(document.getElementById("txtIdPrecioDos").value);
+    precio3 = parseInt(document.getElementById("txtIdPrecioTres").value);
+
+    iva = (precio1 + precio2 + precio3) * 0.21;
+
+    precioFinal = precio1 + precio2 + precio3 + iva;
+    
+    alert("El precio final es de: " + precioFinal);
 }
